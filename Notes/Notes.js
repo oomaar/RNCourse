bf5804fc45f9e84c11648b44fcb890037f1e514d
oomaar <omaar_5@hotmail.com>
2022-05-23 02:48:03 +0200

    Update Notes

---
+++ Notes/Notes.js
@@ -0,0 +1,41 @@
+// React Native Crash Course | Build a Complete App
+// https://www.youtube.com/watch?v=VozPNrt-LfE
+
+// Chapters:-
+// --------
+
+// DONE: 00:00 Welcome!
+// Review: 00:32 What is React Native?
+// Review: 03:58 A look under the hood of React Native
+// Review: 09:04 Using Expo vs the React Native CLI
+// Review: 15:00 Creating a new React Native project
+// Review: 20:50 Analyzing the created project
+// Review: 26:05 Running the first app on a real device
+// Review: 30:26 Setting up a local development environment
+// Review: 39:52 Getting started with React Native basics
+// Review: 41:37 Exploring core components & styling
+// Review: 49:30 Working with core components
+// TODO: 56:30 Styling React Native apps
+// TODO: 01:06:43 Exploring layouts & flexbox
+// TODO: 01:10:03 Combining React Native & flexbox
+// TODO: 01:12:34 Using flexbox in React Native apps
+// TODO: 01:17:42 Flexbox - a deep dive
+// TODO: 01:30:02 Improving the app's layout
+// TODO: 01:37:14 Handling (user) events
+// TODO: 01:44:43 Managing list data
+// TODO: 01:51:54 Platform-specific styling
+// TODO: 02:00:45 Using the ScrollView component for scrolling
+// TODO: 02:07:00 Optimized list handling with FlatList
+// TODO: 02:20:42 Splitting up components
+// TODO: 02:25:53 Working with props & components
+// TODO: 02:28:27 Handling user input
+// TODO: 02:38:06 Building custom buttons
+// TODO: 02:41:40 Deleting items
+// TODO: 02:46:59 Styling custom buttons (Ripple effect on Android)
+// TODO: 02:51:51 Adding a modal screen
+// TODO: 02:58:49 Styling the modal
+// TODO: 03:04:30 Controlling the modal visibility
+// TODO: 03:07:01 Controlling colors & displaying images
+// TODO: 03:13:05 Final touches & finishing the first app
+// TODO: 03:20:00 Wrap up & summary
+// TODO: 03:23:30 Some personal notes
