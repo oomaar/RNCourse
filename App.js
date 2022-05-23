ca59f8778a851ec28882056a17e315ed687c0fc0
oomaar <omaar_5@hotmail.com>
2022-05-23 02:44:28 +0200

    explore core components of react native

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Hello World</Text>
+      </View>
+      <Button title="Click Me !" />
     </View>
   );
 }
