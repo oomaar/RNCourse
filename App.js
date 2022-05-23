0c942e988ffc48912e0590787d34fe6ac71e6da0
oomaar <omaar_5@hotmail.com>
2022-05-23 21:17:55 +0200

    clean up the project

--- App.js
+++ App.js
@@ -2,28 +2,7 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import { TestComponent } from "./components";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Hello World</Text>
-      </View>
-      <Text style={styles.dummyText}>Hello World Two</Text>
-      <Button title="Click Me !" />
-    </View>
-  );
+  return <View></View>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dummyText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "blue",
-    padding: 16,
-  },
-});
+const styles = StyleSheet.create({});
