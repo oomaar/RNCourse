80eddaadd78c535f9d1ef9e1b80f45d8dc6f5c60
oomaar <omaar_5@hotmail.com>
2022-05-23 21:32:19 +0200

    basic use of Flex Box

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
-import { StyleSheet, Text, View, Button } from "react-native";
-import { TestComponent } from "./components";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="What Todo ?" />
+        <Button title="Add Todo" />
+      </View>
+      <View>
+        <Text>List of Todoos...</Text>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
