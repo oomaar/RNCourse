c118475b0ce3a1d634a91752092b265ac1e3da91
oomaar <omaar_5@hotmail.com>
2022-05-24 16:47:51 +0200

    [WIP] state change

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [value, onChangeValue] = useState("");
+  const [state, onChangeState] = useState(false);
+  console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ value", value);
+  console.log("🚀 ~ file: App.js ~ line 8 ~ App ~ state", state)
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <Text style={styles.textInputLabel}>What Todo ?</Text>
-        <TextInput style={styles.textInput} />
+        <Text style={!state ? styles.textInputLabelHide : styles.textInputLabel}>What Todo ?</Text>
+        <TextInput
+          style={styles.textInput}
+          value={value}
+          onChangeText={(e) => {
+            onChangeValue(e);
+            onChangeState(true);
+
+            if (value === "") {
+              onChangeState(false);
+            }
+          }}
+        />
         <Button title="Add Todo" />
       </View>
       <View style={styles.todosContainer}>
@@ -36,6 +53,9 @@ const styles = StyleSheet.create({
     left: "2%",
     margin: 1,
     color: "#ccc"
+  },
+  textInputLabelHide: {
+    width: "0%"
   },
   textInput: {
     borderWidth: 1,
